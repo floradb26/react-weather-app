e74200d49607b11baf81f18b7cca1b30acb97874
floradb26 <debruyne.f@outlook.com>
2021-12-05 11:24:25 +0100

    Added GitHub repository link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-        Weather App
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Floriane Ramirez De Bruyne and is {" "}
+        <a href="https://github.com/floradb26/react-weather-app" target="_blank">open-sourced on GitHub</a>
+        </footer>
     </div>
   );
 }
