98607c392fb1aa5e6f858a9bbb1511e3d16c8fa2
floradb26 <debruyne.f@outlook.com>
2021-12-05 14:57:33 +0100

    Addition of axios

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container"> 
-        <Weather />
+        <Weather defaultCity="Brussels" />
         <footer>
           This project is created by Floriane Ramirez De Bruyne and is {" "}
         <a href="https://github.com/floradb26/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
