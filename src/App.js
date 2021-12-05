5a6c071ab4e77ff2612f9c8d0aed51afacaad749
floradb26 <debruyne.f@outlook.com>
2021-12-05 12:04:20 +0100

    SEO update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <h1>Weather App</h1>
         <footer>
           This project is created by Floriane Ramirez De Bruyne and is {" "}
-        <a href="https://github.com/floradb26/react-weather-app" target="_blank">open-sourced on GitHub</a>
+        <a href="https://github.com/floradb26/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
         </footer>
     </div>
   );
